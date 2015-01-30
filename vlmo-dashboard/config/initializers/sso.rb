9e575cb3101090101575789200f20980846e6f87
chinh-nguyen-ecep <chinh.nguyen@ecepvn.org>
2015-01-30 13:57:57 +0700

    Deleted vlmo-dashboard folder

--- vlmo-dashboard/config/initializers/sso.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/sso.rb
-require 'user'
-Bsm::Sso::Client.configure do |c|
-  c.site = "https://sso.vervemobile.com"
-  c.secret = "Xf28xNCYcwJPtcCxLvw8FF3otWbSbWmEOPiJTUfqkm6oSNwH8yHRKXjthaco"
-  c.user_class = ::User
-end
